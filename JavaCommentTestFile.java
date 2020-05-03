aaa4685800b603c86b8bd1089e1b3d18ed60297b
michaelplatt07 <michael.platt.07@gmail.com>
2020-05-03 14:47:40 -0400

    MAP : Adding test file for comment for java and updating the formatting function.

---
+++ JavaCommentTestFile.java
@@ -0,0 +1,21 @@
+/**
+ * @author: MAPlatt
+ * @date: 2020-05-02
+ * @license:
+ */
+
+public class JavaCommentTestFile {
+
+    // This is a short commment and shouldn't be formmated.
+    public JavaCommentTestFile() {
+	int a = 1;
+	int b = 2;
+    }
+
+    // This is a super serious long comment that is over sixty 
+    // characters in length and should be formmated and brough 
+    // to multiple lines because that is what is necessary.
+    private int add(int a, int b) {
+	return a + b;
+    }
+}
